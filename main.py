a9a1925b3bb6aee384d86640760b287cdadb75a7
jtrtk <j.trytek685@kodland.team>
2024-11-02 15:40:39 +0100

    Create main.py
    asadasddas

---
+++ main.py
@@ -0,0 +1,9 @@
+Oto, co powinieneś/aś zrobić:
+
+Czy masz już repozytorium dla swojego projektu? Utwórz jeden, jeśli nie!
+
+Utwórz nowy plik programu w repozytorium. Możesz to nazwać main.py
+
+Wklej swój kod w tym pliku i naciśnij zatwierdzenie! 
+
+Wszystko gotowe - możesz skopiować link i wysłać go na czat!
