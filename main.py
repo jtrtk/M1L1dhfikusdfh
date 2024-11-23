365a78ee8251aa2b5bcbd913fce2634d7ee328c2
jtrtk <j.trytek685@kodland.team>
2024-11-23 15:44:06 +0100

    Update main.py
    wdadsasdsda

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ Utwórz nowy plik programu w repozytorium. Możesz to nazwać main.py
 Wklej swój kod w tym pliku i naciśnij zatwierdzenie! 
 
 Wszystko gotowe - możesz skopiować link i wysłać go na czat!
+ adsiufskidugukidsugdkfis\d\s
